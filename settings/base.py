f4a60c7d9820a99eff1cfa1ab7d6b69c35fa8b7a
Daniel da Silva Farias <ddanielsilva661@gmail.com>
2018-12-01 22:42:40 -0300

    [DONE] Adding Rest Framework Config

--- settings/base.py
+++ settings/base.py
@@ -67,6 +67,14 @@ DATABASES = {
     ),
 }
 
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
